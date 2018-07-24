8126ef53cb11c4882e6245ead5b12fbd07252439
Safiul <safiul.sadi@northsouth.edu>
2018-07-24 18:22:20 +0600

    manis

--- app/src/main/java/com/example/safiul/anissproject/MainActivity.java
+++ app/src/main/java/com/example/safiul/anissproject/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
     private TextView ageTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        //command linje
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         nameTextView = (TextView) findViewById(R.id.nameTextViewId);
